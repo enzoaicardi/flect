c1dc3246c2d97f9edb5d7cefa0ab8d4a98b70a67
enzoaicardi <enzo.aicardi@gmail.com>
2023-07-01 18:14:17 +0200

    rename

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import terser from '@rollup/plugin-terser';
 export default {
 	input: 'src/bundle.js',
 	output: {
-		file: 'dist/composite.js',
+		file: 'dist/flect.js',
 		format: 'iife'
 	},
     plugins: [terser()]
