6de0526c2e3ecdcec3945ba5095c53ce407ab1f1
enzoaicardi <enzo.aicardi@gmail.com>
2023-10-08 20:55:35 +0200

    update on forloop

--- refactorv2/action/transformer.js
+++ refactorv2/action/transformer.js
@@ -58,7 +58,6 @@ function updateForAction(_, element, path){
     }
 
     else if(gap < 0){
-        // transformerRemove.call(this, element, element._xcount + gap)
         for(let x = count - 1; (x > (count - 1) + gap) && x >= 0; x--){
             transformerRemove.call(this, element, map, x)
         }
@@ -70,7 +69,8 @@ function updateForAction(_, element, path){
 
 function setTransformerDefaults(element){
     element._xmatches || (element._xmatches = [])
-    element._xbounds || (element._xbounds = {})
+    // TODO : delete this if unused
+    // element._xbounds || (element._xbounds = {})
     element._xbinded || (element._xbinded = {})
     element._xcount || (element._xcount = 0)
 }
@@ -83,10 +83,8 @@ function transformerAppend(element, map, index, path){
         !path || (clone._xmatches.set(path, index))
 
     // add rootElement reference to boundary
-    // element._xbounds[index] = clone.firstChild
     clone.firstChild.rootElement = map.rootElement
-    // TODO : remove multiple nodes at same time
-    // sinon faire le removechild classique mais utiliser les bornes sans passer par rootElement
+    // TODO : voir si pas mieux de ne pas passer par le rootelement property
 
     // bind fragment
     element._xbinded[index] = this.bindElements(clone, map)
@@ -100,16 +98,6 @@ function transformerRemove(element, map, index){
 
     let sibling = {}
     let parent = element.parentNode
-
-    // let boundary = element._xbounds[index]
-
-    // while(sibling !== element && (sibling = boundary.nextSibling)){
-    //     parent.removeChild(sibling)
-    // }
-
-    // TODO -> replace le parent par un commentaire (ou element cloné [pas deep] lui meme)
-    // effectuer les modifications hors du dom (suppressions)
-    // replace l'element faux par l'ancien element avec les modifs
 
     while((!sibling.rootElement || sibling.rootElement !== map.rootElement) && (sibling = element.previousSibling)){
         parent.removeChild(sibling)
