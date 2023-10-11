1418decbd7e4a6f6f234a0ea422969a0faf2e600
Enzo AICARDI <eaicardi@datasolution.fr>
2023-10-11 10:52:51 +0200

    update regex + test

--- tests/refactor.js
+++ tests/refactor.js
@@ -5,18 +5,12 @@ Flect.define('test', class extends Flect.x {
 
     init(){
 
-        this.x.item = {
-            name: 'haha',
-            price: {
-                tax: 10,
-                brut: 20
-            }
-        }
+        this.x.item = [[1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
         console.log(this.x.item)
 
         setTimeout(()=>{
-            this.x.item.price.tax = {}
+            this.x.item[0].unshift(3)
         }, 200)
 
     }
