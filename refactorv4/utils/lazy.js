eaaceaba153c42d6e9765219a13d327b0ca095b9
enzoaicardi <enzo.aicardi@gmail.com>
2023-12-10 12:06:18 +0100

    lazy update memo

--- refactorv4/utils/lazy.js
+++ refactorv4/utils/lazy.js
@@ -2,4 +2,8 @@
     charge paresseusement un élement lorsque que ce dernier est détecté dans le DOM
     cela permet de mettre en place un routeur sans surcharge
     utile de faire via intersectionobserver ? aucune idée
+
+    Flect.lazy({
+        elementName: ()=>import('the url of the element .define')
+    })
 */
