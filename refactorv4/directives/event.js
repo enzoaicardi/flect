98a15587427fdb4f377ecbd2f3a85c6636176b64
Enzo AICARDI <eaicardi@datasolution.fr>
2023-12-20 14:24:30 +0100

    attach handler to element

--- refactorv4/directives/event.js
+++ refactorv4/directives/event.js
@@ -3,9 +3,15 @@
  * @type {xDirective}
  */
 export function eventDirective(context, element, expression, attributeName) {
-    // create the event handler object
-    const eventHandler = (expression.handler =
-        expression.handler || new xHandler());
+    /**
+     * create the event handler object
+     * we store it on the element for memory reasons
+     * because when the element is removed from the DOM
+     * the handler is deleted by the garbage collector
+     * @type {xHandler}
+     */
+    const eventHandler = (element.handler =
+        element.handler || new xHandler(context));
 
     // assign the event handler method
     eventHandler[attributeName] = expression(context);
@@ -16,7 +22,11 @@ export function eventDirective(context, element, expression, attributeName) {
 
 /** @type {xHandler} */
 export class xHandler {
+    constructor(context) {
+        /** @type {xDatas} */
+        this.context = context;
+    }
     handleEvent(event) {
-        this[event.type](event);
+        this[event.type].call(context, event);
     }
 }
