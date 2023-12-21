8e2e572e0adc124e62b26415e98d0ea01ae24d79
Enzo AICARDI <eaicardi@datasolution.fr>
2023-12-21 17:13:36 +0100

    todo memo

--- refactorv4/directives/for.js
+++ refactorv4/directives/for.js
@@ -2,4 +2,6 @@
     Directive executée depuis template x-for=""
     reprend le template.content et s'en sert pour faire du clonnage d'éléments
     Fait partie des attr directives
+
+    -> regarder chez lit.js le tetes et queues -> algorithme pour ajouter, modifier ou supprimer des elements efficacement
 */
