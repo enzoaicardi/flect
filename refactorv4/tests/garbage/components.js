c693e480b61e355180fd5db21f7efaa29de9460e
enzoaicardi <enzo.aicardi@gmail.com>
2024-01-12 23:10:57 +0100

    test nested components perfs

--- refactorv4/tests/garbage/components.js
+++ refactorv4/tests/garbage/components.js
@@ -9,46 +9,64 @@ const prev = [...Array(8000)].map((_) => Math.ceil(Math.random() * 10));
 const next = [...Array(20)].map((_) => Math.ceil(Math.random() * 1000));
 const final = [...Array(4000)].map((_) => Math.ceil(Math.random() * 1000));
 
-// let o = performance.now();
+let o = performance.now();
 
-// function freeze() {
-//     setTimeout(() => {
-//         if (performance.now() - o > 30) {
-//             console.log("freeze : " + (performance.now() - o));
-//         }
-//         o = performance.now();
-//         freeze();
-//     }, 1);
-// }
+function freeze() {
+    setTimeout(() => {
+        if (performance.now() - o > 30) {
+            console.log("freeze : " + (performance.now() - o));
+        }
+        o = performance.now();
+        freeze();
+    }, 1);
+}
 
-// freeze();
+freeze();
 
 setTimeout(() => {
     // console.log("lit");
-    // class BasicElement extends LitElement {
-    //     static properties = {
-    //         array: {},
-    //     };
-    //     constructor() {
-    //         super();
-    //         this.array = prev;
-    //         setTimeout(() => {
-    //             this.array = next;
-    //         }, 1000);
-    //         setTimeout(() => {
-    //             this.array = final;
-    //         }, 2000);
-    //     }
-    //     render() {
-    //         return html`
-    //             <ul>
-    //                 ${this.array.map((item) => html`<li>${item}</li>`)}
-    //             </ul>
-    //         `;
-    //     }
-    // }
-    // customElements.define("basic-element", BasicElement);
+    class OtherElement extends LitElement {
+        static properties = {
+            text: {},
+        };
+        constructor() {
+            super();
+            this.text = "";
+        }
+        render() {
+            return html`<li>${this.text}</li>`;
+        }
+    }
+    customElements.define("basic-li", OtherElement);
+    class BasicElement extends LitElement {
+        static properties = {
+            array: {},
+        };
+        constructor() {
+            super();
+            this.array = prev;
+            setTimeout(() => {
+                this.array = next;
+            }, 1000);
+            setTimeout(() => {
+                this.array = final;
+            }, 2000);
+        }
+        render() {
+            return html`
+                <ul>
+                    ${this.array.map(
+                        (item) => html`<basic-li .text="${item}"></basic-li>`
+                    )}
+                </ul>
+            `;
+        }
+    }
+    customElements.define("basic-element", BasicElement);
     // console.log("flect");
+    // Flect.define("li", function (signal, html) {
+    //     return html`<li x-text="item()"></li>`;
+    // });
     // Flect.define("p", function (signal, html, css) {
     //     this.array = signal(prev);
     //     // this.array = signal([1, 2, 3, 4, 5]);
@@ -62,77 +80,78 @@ setTimeout(() => {
     //     }, 2000);
     //     return html` <ul>
     //         <template x-for="array()">
-    //             <li x-text="item()"></li>
+    //             <x-li x-item="item()"></x-li>
     //         </template>
     //     </ul>`;
     // });
 }, 1000);
 
-Flect.observe({
-    "x-two": () => console.log("x-two found"),
-});
+// Flect.observe({
+//     "x-one": () => console.log("x-two found"),
+// });
 
-setTimeout(() => {
-    Flect.define("two", function (signal, html, css) {
-        this.text = signal("x-two -> success");
-        this.array = signal([1]);
+// setTimeout(() => {
+//     Flect.define("two", function (signal, html, css) {
+//         this.text = signal("x-two -> success");
+//         this.array = signal([1]);
 
-        this.navigate = Flect.navigate;
+//         this.navigate = Flect.navigate;
+//         console.log("found in dom");
 
-        setTimeout(() => {
-            this.array([]);
-        }, 50);
-        setTimeout(() => {
-            this.array([1, 2, 3]);
-        }, 500);
+//         setTimeout(() => {
+//             this.array([]);
+//         }, 50);
+//         setTimeout(() => {
+//             this.array([1, 2, 3]);
+//         }, 500);
 
-        setTimeout(() => {
-            this.text("x-two -> success update");
-        }, 1000);
+//         setTimeout(() => {
+//             this.text("x-two -> success update");
+//         }, 1000);
 
-        css`
-            [${css("bg")}]::before {
-                background: green;
-                content: "(TWO)";
-                margin-right: 10px;
-            }`;
+//         css`
+//             [${css("bg")}]::before {
+//                 background: green;
+//                 content: "(TWO)";
+//                 margin-right: 10px;
+//             }`;
 
-        return this.component.childNodes;
-    });
-}, 500);
+//         return this.component.childNodes;
+//     });
+// }, 500);
 
-setTimeout(() => {
-    Flect.define("three", function (signal, html, css) {
-        this.text = this.text || (() => {});
+// setTimeout(() => {
+//     Flect.define("three", function (signal, html, css) {
+//         this.text = this.text || (() => {});
 
-        // setTimeout(() => {
-        //     this.text("x-three -> success update");
-        // }, 1000);
+//         // setTimeout(() => {
+//         //     this.text("x-three -> success update");
+//         // }, 1000);
 
-        css`
-            [${css("bg")}]::before {
-                background: yellow;
-                content: "(THREE)";
-                margin-right: 10px;
-            }`;
+//         css`
+//             [${css("bg")}]::before {
+//                 background: yellow;
+//                 content: "(THREE)";
+//                 margin-right: 10px;
+//             }`;
 
-        return this.component.childNodes;
-    });
-}, 1000);
+//         return this.component.childNodes;
+//     });
+// }, 1000);
 
-Flect.define("one", function (signal, html, css) {
-    // this.text = this.text || signal("x-one -> success");
+// Flect.define("one", function (signal, html, css) {
+//     // this.text = this.text || signal("x-one -> success");
 
-    // setTimeout(() => {
-    //     this.text("x-one -> success update");
-    // }, 1000);
+//     // setTimeout(() => {
+//     //     this.text("x-one -> success update");
+//     // }, 1000);
 
-    css`
-        [${css("bg")}]::before {
-            background: red;
-            content: "(ONE)";
-            margin-right: 10px;
-        }`;
+//     css`
+//         [${css("bg")}]::before {
+//             background: red;
+//             content: "(ONE)";
+//             margin-right: 10px;
+//         }`;
 
-    return this.component.childNodes;
-});
+//     return this.component.childNodes;
+// });
