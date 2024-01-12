b3e2b68ed5f257445af0d58c33746b1f4957aefb
Enzo AICARDI <eaicardi@datasolution.fr>
2024-01-12 16:50:39 +0100

    move into feature folder

---
+++ refactorv4/features/observe.js
@@ -0,0 +1,11 @@
+/*
+    charge paresseusement un élement lorsque que ce dernier est détecté dans le DOM
+    cela permet de mettre en place un routeur sans surcharge
+    utile de faire via intersectionobserver ? aucune idée
+
+    Ne le faire qu'une fois !
+
+    Flect.lazy({
+        elementName: ()=>import('the url of the element .define')
+    })
+*/
